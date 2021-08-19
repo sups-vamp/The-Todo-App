0fc485905515affa94ad0f141bba5047d52bf6f4
supriya das <41366010+sups-vamp@users.noreply.github.com>
2021-08-20 01:28:28 +0530

    Add files via upload

---
+++ todo/src/App.js
@@ -0,0 +1,12 @@
+import TodoScreen from "./Components/todoScreen/TodoScreen";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoScreen></TodoScreen>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/Components/completedItems/CompletedTodo.jsx
@@ -0,0 +1,17 @@
+import React,{useRef} from "react";
+import "./CompletedTodo.css";
+
+export default function CompletedTodo(props) {
+    const refTodo = useRef('');
+    const handleAdd=()=>{
+        props.Add(refTodo.current.innerHTML);
+      }
+  return (
+    <div class="completed-tasks-main">
+      <i className="fa fa-ellipsis-v"></i>
+      <i className="fa fa-ellipsis-v"></i>
+      <input type="radio" name="todo" onClick={handleAdd}></input>
+      <h4 class="completed-item" ref={refTodo}>{props.completedItem}</h4>
+    </div>
+  );
+}
